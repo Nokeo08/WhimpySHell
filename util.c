a51465b3251d1d502e7c39d0237662a09f96b0e4
cahlen <cahlen@iastate.edu>
2015-10-07 20:35:48 -0500

    Removed unnecessary inclusions from util.c

--- util.c
+++ util.c
@@ -1,11 +1,6 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <ctype.h>
-#include <unistd.h>
-
-#include <sys/types.h>
-#include <sys/wait.h>
+#include <string.h> /* strchr */
+#include <ctype.h> /* isspace */
+#include <sys/wait.h> /* wait */
 
 #include "util.h"
 
